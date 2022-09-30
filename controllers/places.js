40df59b4213af33e8a04f0b2b4c777759fe77995
Cassandra Collins <103472275+CassandraRose12@users.noreply.github.com>
2022-09-30 16:53:17 -0400

    form data getting through

--- controllers/places.js
+++ controllers/places.js
@@ -5,21 +5,27 @@ router.get('/', (req, res) => {
         name: 'Le Diplomate',
         city: 'District of Columbia',
         state: 'D.C.',
-        cuisines: 'French, French-American',
+        cuisines: 'French',
         pic: '/images/le-dip.jpg'
       }, {
         name: 'Exiles Bar',
         city: 'District of Columbia',
         state: 'D.C.',
-        cuisines: 'Pub, American',
+        cuisines: 'American',
         pic: '/images/exiles.jpg'
       }]
       
-    res.render('places/index', { places })
+    res.render('places/index', {places })
 })
 router.get('/new', (req, res) => {
     res.render('places/new')
   })
+
+  router.post('/', (req, res) => {
+    console.log(req.body)
+    res.send('POST /places')
+  })
+  
   
 
 module.exports = router
