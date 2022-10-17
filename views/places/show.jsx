4de9b3f7dea4f962534e81c90ff2cd64fcd05e22
Cassandra Collins <103472275+CassandraRose12@users.noreply.github.com>
2022-10-16 20:10:47 -0400

    updated show page

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,3 +1,4 @@
+
 const React = require('react')
 const Def = require('../default')
 
@@ -5,15 +6,34 @@ function show (data) {
     return (
         <Def>
           <main>
+            <div className="row">
+              <div className="col-sm-6">
+                <img src={data.place.pic} alt={data.place.name}/>
+                <h3>
+                  Located in {data.place.city}, {data.place.state}
+                </h3>
+              </div>
+              <div className="col-sm-6">
+                <h2>Description</h2>
+                <h3>
+                  {data.place.showEstablished()}
+                </h3>
+                <h4>
+                  Serving {data.place.cuisines}
+                </h4>
+              </div>
             <h1>{ data.place.name }</h1>
             <p>Currently Unrated</p>
             <p>Comments</p>
+            <div>
             <a href={`/places/${data.id}/edit`} className="btn btn-warning">
                 Edit
             </a>
             <form method="POST" action={`/places/${data.id}/delete?_method=DELETE`}>
                 <button type="submit" className=" btn btn-danger">Delete</button>
             </form>
+            </div>
+            </div>
           </main>
         </Def>
     )
